b57ad0621fe9fb7a5d3f52ad07efd5f8e80e0e85
Daniel Torren Peraire <1621511@uab.cat>
2023-05-22 18:40:21 +0200

    still not working

--- package/model/individuals.py
+++ package/model/individuals.py
@@ -65,68 +65,17 @@ class Individual:
             self.history_low_carbon_preferences = [self.low_carbon_preferences]
             self.history_identity = [self.identity]
             self.history_flow_carbon_emissions = [self.flow_carbon_emissions]
-##############################################################
-    #I would like to make theses three functions, where the last calls the second and the second calls the first faster:
-    def calc_chi_old(self, a, P_L, A, omega, sigma, p, m):
-        
-        part_one = ((a[m] * P_L[p] * A[m] * omega[p]**(1/sigma[p]))/(a[p] * P_L[m] * A[p] * omega[m]**(1/sigma[m])))**(self.service_substitutability/(1+self.service_substitutability))
 
-        #if np.isnan(np.sum(part_one)):
-        #    print("part one bits", (a[m] * P_L[p] * A[m] * omega[p]**(1/sigma[p])) ,(a[p] * P_L[m] * A[p] * omega[m]**(1/sigma[m])))
-        #    print("self.low_carbon_preferences", self.low_carbon_preferences)
-        #    print("self.prices_high_carbon_instant", self.prices_high_carbon_instant)
-        #    quit()
-        #print("part_one", part_one)
-        part_two = (A[p] * omega[p]**((sigma[p]-1)/sigma[p]) + (1 - A[p]))**((sigma[p] - self.service_substitutability)/((sigma[p] - 1) * self.service_substitutability))
-        #if np.isnan(np.sum(part_two)):
-        #    print("part one bits",A[p] * omega[p]**((sigma[p]-1)/sigma[p]) + (1 - A[p]) ,((sigma[p] - self.service_substitutability)/((sigma[p] - 1) * self.service_substitutability)))
-        #    print("self.low_carbon_preferences", self.low_carbon_preferences)
-        #    print("self.prices_high_carbon_instant", self.prices_high_carbon_instant)
-        #    quit()
-        #print("part_two", part_two)
-        part_three = (A[m] * omega[m]**((sigma[m]-1)/sigma[m]) + (1 - A[m]))**((sigma[m] - self.service_substitutability)/((sigma[m] - 1) * self.service_substitutability))
-        #if np.isnan(np.sum(part_three)):
-        #    print("part one bits",  (A[m] * omega[m]**((sigma[m]-1)/sigma[m]) + (1 - A[m])) ,((sigma[m] - self.service_substitutability)/((sigma[m] - 1) * self.service_substitutability)))
-        #    print("self.low_carbon_preferences", self.low_carbon_preferences)
-        #    print("self.prices_high_carbon_instant", self.prices_high_carbon_instant)
-        #    quit()
-        #print("part_three", pat_three)
-        chi = part_one*part_two/part_three
-        #print("chi", chi)
-        return chi
-
-    def calc_chi_matrix_old(self):
-        chi_matrix = []
-        for i in range(self.M):
-            chi_row = []
-            for j in range(self.M):
-                value_chi = self.calc_chi_old(self.service_preferences, self.prices_low_carbon, self.low_carbon_preferences, self.Omega_m, self.low_carbon_substitutability_array, i, j)
-                chi_row.append(value_chi)#goes p then m
-            chi_matrix.append(chi_row)
-
-        return np.asarray(chi_matrix)
-    
-    def calc_H_m_denominator_old(self, m):
-        return sum([self.chi_matrix[p][m]*(self.Omega_m[p]*self.prices_low_carbon[p] + self.prices_high_carbon_instant[p]) for p in range(self.M)])
-         
-
-    def calc_consumption_quantities_old(self):
-        H_m_denominators = np.asarray([self.calc_H_m_denominator_old(m) for m in range(self.M)])
-        H_m = self.instant_budget/H_m_denominators
-        L_m = H_m*self.Omega_m
-        return H_m,L_m
-############################################################
     def calc_omega(self):        
-        omega_vector = ((self.prices_high_carbon_instant* self.low_carbon_preferences)/(self.prices_low_carbon*(1- self.low_carbon_preferences )))**(self.low_carbon_substitutability_array)
+        omega_vector = ((self.prices_high_carbon_instant*self.low_carbon_preferences)/(self.prices_low_carbon*(1- self.low_carbon_preferences )))**(self.low_carbon_substitutability_array)
         return omega_vector
 
-    def calc_chi_components(self, a, P_H, A, omega, sigma):
-        return ((P_H/a)**(self.service_substitutability))*(A * omega**((sigma-1)/sigma) + (1 - A))**((1-self.service_substitutability)*((sigma)/((sigma - 1))))
-    
     def calc_chi_matrix(self):
-        chi_components = self.calc_chi_components(self.service_preferences, self.prices_high_carbon_instant, self.low_carbon_preferences, self.Omega_m, self.low_carbon_substitutability_array)      
-        if np.isnan(np.sum(chi_components)):
-            print(" chi_components", chi_components)
+
+        chi_components = ((self.prices_high_carbon_instant/self.service_preferences)**(self.service_substitutability))*((self.low_carbon_preferences*(self.Omega_m**((self.low_carbon_substitutability_array-1)/self.low_carbon_substitutability_array)) + (1 - self.low_carbon_preferences))**((1-self.service_substitutability)*((self.low_carbon_substitutability_array)/((self.low_carbon_substitutability_array - 1)))))
+
+        print("chi_components", chi_components)
+
         A, B = np.meshgrid(chi_components, chi_components)
         chi_matrix = A/B
 
