c4b7d736eaa1b94c3a95c9b401b8a6182d48605d
Daniel Torren Peraire <1621511@uab.cat>
2024-05-16 11:46:15 +0200

    1e-3

--- package/generating_data/single_experiment_gen.py
+++ package/generating_data/single_experiment_gen.py
@@ -37,7 +37,51 @@ def main(
 
 if __name__ == '__main__':
     
-    base_params = {'save_timeseries_data_state': 1, 'compression_factor_state': 1, 'heterogenous_intrasector_preferences_state': 1, 'heterogenous_carbon_price_state': 1, 'heterogenous_sector_substitutabilities_state': 1, 'SBM_block_heterogenous_individuals_substitutabilities_state': 0, 'heterogenous_phi_state': 0, 'imperfect_learning_state': 1, 'imitation_state': 'consumption', 'alpha_change_state': 'dynamic_identity_determined_weights', 'vary_seed_state': 'network', 'seed_reps': 25, 'network_structure_seed': 8, 'preferences_seed': 14, 'shuffle_homophily_seed': 20, 'learning_seed': 10, 'carbon_price_duration': 1000, 'burn_in_duration': 0, 'M': 2, 'clipping_epsilon_init_preference': 1e-05, 'init_carbon_price': 0, 'BA_nodes': 11, 'BA_green_or_brown_hegemony': 0, 'SBM_block_num': 2, 'SBM_network_density_input_intra_block': 0.2, 'SBM_network_density_input_inter_block': 0.005, 'SW_network_density': 0.1, 'SW_prob_rewire': 0.1, 'network_type': 'SW', 'phi_lower': 0.79818115234375, 'carbon_price_increased_lower': 1.568603515625, 'carbon_price_increased_upper': 3.670654296875, 'N': 399.0, 'sector_substitutability': 2.3463623046875, 'low_carbon_substitutability_lower': 3.9688232421875, 'low_carbon_substitutability_upper': 4.7134033203125, 'confirmation_bias': 21.13037109375, 'homophily_state': 0.856201171875, 'a_identity': 2.4819580078125, 'b_identity': 7.4580322265625, 'std_low_carbon_preference': 0.2073486328125, 'set_seed': 1}
+    #base_params = {'save_timeseries_data_state': 1, 'compression_factor_state': 1, 'heterogenous_intrasector_preferences_state': 1, 'heterogenous_carbon_price_state': 1, 'heterogenous_sector_substitutabilities_state': 1, 'SBM_block_heterogenous_individuals_substitutabilities_state': 0, 'heterogenous_phi_state': 0, 'imperfect_learning_state': 1, 'imitation_state': 'consumption', 'alpha_change_state': 'dynamic_identity_determined_weights', 'vary_seed_state': 'network', 'seed_reps': 25, 'network_structure_seed': 8, 'preferences_seed': 14, 'shuffle_homophily_seed': 20, 'learning_seed': 10, 'carbon_price_duration': 1000, 'burn_in_duration': 0, 'M': 2, 'clipping_epsilon_init_preference': 1e-05, 'init_carbon_price': 0, 'BA_nodes': 11, 'BA_green_or_brown_hegemony': 0, 'SBM_block_num': 2, 'SBM_network_density_input_intra_block': 0.2, 'SBM_network_density_input_inter_block': 0.005, 'SW_network_density': 0.1, 'SW_prob_rewire': 0.1, 'network_type': 'SW', 'phi_lower': 0.79818115234375, 'carbon_price_increased_lower': 1.568603515625, 'carbon_price_increased_upper': 3.670654296875, 'N': 399.0, 'sector_substitutability': 2.3463623046875, 'low_carbon_substitutability_lower': 3.9688232421875, 'low_carbon_substitutability_upper': 4.7134033203125, 'confirmation_bias': 21.13037109375, 'homophily_state': 0.856201171875, 'a_identity': 2.4819580078125, 'b_identity': 7.4580322265625, 'std_low_carbon_preference': 0.2073486328125, 'set_seed': 1}
+    base_params = {
+    "phi_lower": 1e-2,
+    "network_type": "SW",
+    "carbon_price_increased_lower": 0,
+    "save_timeseries_data_state": 1,
+    "compression_factor_state": 1,
+    "heterogenous_intrasector_preferences_state": 1,
+    "heterogenous_carbon_price_state": 0,
+    "heterogenous_sector_substitutabilities_state": 0,
+    "SBM_block_heterogenous_individuals_substitutabilities_state": 0,
+    "heterogenous_phi_state": 0,
+    "imperfect_learning_state": 1,
+    "imitation_state": "consumption",
+    "vary_seed_state": "network",
+    "alpha_change_state": "dynamic_identity_determined_weights",
+    "seed_reps": 25,
+    "network_structure_seed": 10, 
+    "preferences_seed": 77, 
+    "shuffle_homophily_seed": 2,
+    "shuffle_coherance_seed": 5,
+    "set_seed": 4, 
+    "carbon_price_duration": 1000, 
+    "burn_in_duration": 0, 
+    "N": 200, 
+    "M": 2, 
+    "sector_substitutability": 2, 
+    "low_carbon_substitutability_lower": 2, 
+    "low_carbon_substitutability_upper": 2, 
+    "a_identity": 2, 
+    "b_identity": 2, 
+    "clipping_epsilon_init_preference": 1e-5,
+    "std_low_carbon_preference": 0.01, 
+    "confirmation_bias": 5, 
+    "init_carbon_price": 0, 
+    "homophily_state": 0,
+    "coherance_state": 0.95,
+    "BA_nodes": 11,
+    "BA_green_or_brown_hegemony": 0,
+    "SBM_block_num": 2,
+    "SBM_network_density_input_intra_block": 0.2,
+    "SBM_network_density_input_inter_block": 0.005,
+    "SW_network_density": 0.1,
+    "SW_prob_rewire": 0.1
+    }
 
     fileName = main(base_params=base_params)
 
--- package/plotting_data/phi_effect_plot.py
+++ package/plotting_data/phi_effect_plot.py
@@ -39,9 +39,78 @@ def plot_emisisons_simple(
     plotName = fileName + "/Plots"
     f = plotName + "/network_emissions_simple_phi"
     fig.savefig(f+ ".png", dpi=600, format="png") 
-    #fig.savefig(f+ ".eps", dpi=600, format="eps")  
+    #fig.savefig(f+ ".eps", dpi=600, format="eps")
+
+def plot_emisisons_simple_xlog(
+    fileName, emissions_networks, scenarios_titles, property_vals, colors_scenarios, network_titles
+):
+
+    #print(c,emissions_final)
+    fig, axes = plt.subplots(ncols = 3, nrows = 1,figsize=(15,8), sharey=True)#
+
+    #colors = iter(rainbow(np.linspace(0, 1,len(emissions_networks[0]))))
+
+    for k, ax in enumerate(axes.flat):
+        emissions  = emissions_networks[k]
+
+        for i in range(len(emissions)):
+            #color = next(colors)#set color for whole scenario?
+            Data = emissions[i]
+            #print("Data", Data.shape)
+            mu_emissions = Data.mean(axis=1)
+            ax.plot(property_vals, mu_emissions, label=scenarios_titles[i], c = colors_scenarios[i])
+            data_trans = Data.T
+            for v in range(len(data_trans)):
+                ax.plot(property_vals, data_trans[v], color = colors_scenarios[i], alpha = 0.1)
+
+        ax.set_xscale('log')
+        #ax.legend()
+        ax.set_xlabel(r"Social susceptability, $\phi$")
+        
+        ax.set_title (network_titles[k])
+    axes[0].set_ylabel(r"Cumulative carbon emissions, E")
+    handles, labels = axes[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=len(scenarios_titles), fontsize="10")
+
+    # plt.tight_layout()
+    plotName = fileName + "/Plots"
+    f = plotName + "/network_emissions_simple_phi_xlog"
+    fig.savefig(f+ ".png", dpi=600, format="png")   
 
 def plot_var_emisisons_simple(
+    fileName, emissions_networks, scenarios_titles, property_vals, colors_scenarios, network_titles
+):
+
+    #print(c,emissions_final)
+    fig, axes = plt.subplots(ncols = 3, nrows = 1,figsize=(15,8))#
+
+    #colors = iter(rainbow(np.linspace(0, 1,len(emissions_networks[0]))))
+
+    for k, ax in enumerate(axes.flat):
+        emissions  = emissions_networks[k]
+
+        for i in range(len(emissions)):
+            #color = next(colors)#set color for whole scenario?
+            Data = emissions[i]
+            #print("Data", Data.shape)
+            var_emissions = Data.var(axis=1)
+            ax.plot(property_vals[1:], var_emissions[1:], label=scenarios_titles[i], c = colors_scenarios[i])
+
+        #ax.legend()
+        ax.set_xlabel(r"Social susceptability, $\phi$")
+        
+        ax.set_title (network_titles[k])
+    axes[0].set_ylabel(r"Variance cumulative carbon emissions, Var(E)")
+    handles, labels = axes[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=len(scenarios_titles), fontsize="10")
+
+    # plt.tight_layout()
+    plotName = fileName + "/Plots"
+    f = plotName + "/network_emissions_simple_phi_var"
+    fig.savefig(f+ ".png", dpi=600, format="png") 
+    #fig.savefig(f+ ".eps", dpi=600, format="eps")  
+
+def plot_var_emisisons_simple_log(
     fileName, emissions_networks, scenarios_titles, property_vals, colors_scenarios, network_titles
 ):
 
@@ -61,6 +130,7 @@ def plot_var_emisisons_simple(
             ax.plot(property_vals, var_emissions, label=scenarios_titles[i], c = colors_scenarios[i])
 
         #ax.legend()
+        ax.set_yscale('log')
         ax.set_xlabel(r"Social susceptability, $\phi$")
         
         ax.set_title (network_titles[k])
@@ -70,9 +140,44 @@ def plot_var_emisisons_simple(
 
     # plt.tight_layout()
     plotName = fileName + "/Plots"
-    f = plotName + "/network_emissions_simple_phi_var"
+    f = plotName + "/network_emissions_simple_phi_var_log"
     fig.savefig(f+ ".png", dpi=600, format="png") 
-    #fig.savefig(f+ ".eps", dpi=600, format="eps")  
+    #fig.savefig(f+ ".eps", dpi=600, format="eps") 
+
+def plot_var_emisisons_simple_xlog(
+    fileName, emissions_networks, scenarios_titles, property_vals, colors_scenarios, network_titles
+):
+
+    #print(c,emissions_final)
+    fig, axes = plt.subplots(ncols = 3, nrows = 1,figsize=(15,8))#
+
+    #colors = iter(rainbow(np.linspace(0, 1,len(emissions_networks[0]))))
+
+    for k, ax in enumerate(axes.flat):
+        emissions  = emissions_networks[k]
+
+        for i in range(len(emissions)):
+            #color = next(colors)#set color for whole scenario?
+            Data = emissions[i]
+            #print("Data", Data.shape)
+            var_emissions = Data.var(axis=1)
+            ax.plot(property_vals, var_emissions, label=scenarios_titles[i], c = colors_scenarios[i])
+
+        #ax.legend()
+        ax.set_xscale('log')
+        ax.set_xlabel(r"Social susceptability, $\phi$")
+        
+        ax.set_title (network_titles[k])
+    axes[0].set_ylabel(r"Variance cumulative carbon emissions, Var(E)")
+    handles, labels = axes[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=len(scenarios_titles), fontsize="10")
+
+    # plt.tight_layout()
+    plotName = fileName + "/Plots"
+    f = plotName + "/network_emissions_simple_phi_var_xlog"
+    fig.savefig(f+ ".png", dpi=600, format="png") 
+    #fig.savefig(f+ ".eps", dpi=600, format="eps") 
+
 
 def main(
     fileName
@@ -90,11 +195,15 @@ def main(
     emissions_array = load_object(fileName + "/Data", "emissions_array")
     scenarios_titles = labels_SW = [r"No carbon price, $\tau = 0$", r"Low carbon price, $\tau = 0.1$", r"High carbon price, $\tau = 1$"]
 
-    plot_emisisons_simple(fileName, emissions_array, scenarios_titles, property_values_list, colors_scenarios, network_titles)
-    plot_var_emisisons_simple(fileName, emissions_array, scenarios_titles, property_values_list, colors_scenarios, network_titles)
+    #plot_emisisons_simple(fileName, emissions_array, scenarios_titles, property_values_list, colors_scenarios, network_titles)
+    plot_emisisons_simple_xlog(fileName, emissions_array, scenarios_titles, property_values_list, colors_scenarios, network_titles)
+    #plot_var_emisisons_simple(fileName, emissions_array, scenarios_titles, property_values_list, colors_scenarios, network_titles)
+    #plot_var_emisisons_simple_log(fileName, emissions_array, scenarios_titles, property_values_list, colors_scenarios, network_titles)
+    plot_var_emisisons_simple_xlog(fileName, emissions_array, scenarios_titles, property_values_list, colors_scenarios, network_titles)
+    
     plt.show()
 
 if __name__ == '__main__':
     plots = main(
-        fileName= "results/phi_vary_17_24_43__15_05_2024",
+        fileName= "results/phi_vary_10_26_48__16_05_2024",
     )
