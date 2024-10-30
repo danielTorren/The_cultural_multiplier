080079f491181e2fc5fc00954b6394023865e39b
Daniel Torren Peraire <1621511@uab.cat>
2024-10-30 13:26:30 +0100

    verison 1.0

--- package/plotting_data/single_experiment_plot.py
+++ package/plotting_data/single_experiment_plot.py
@@ -1116,58 +1116,11 @@ def main(
 
     Data = load_object(fileName + "/Data", "social_network")
 
-    #print("Data EMissions", Data.total_carbon_emissions_stock)
-
-    #node_shape_list = ["o","s","^","v"]
-
-    #anim_save_bool = False#Need to install the saving thing
-    ###PLOTS
-
     plot_network(Data)
     plot_identity_matrix(fileName, Data, dpi_save)
-    #plot_preference_timeseries(fileName, Data, dpi_save)
-
-
-
-    #bin_num= 200
-    #plot_identity_matrix_density(fileName, Data, dpi_save, bin_num)
-    #plot_emissions_flow_matrix(fileName, Data, dpi_save)
-    #plot_emissions_individuals(fileName, Data, dpi_save)
-
+    plot_preference_timeseries(fileName, Data, dpi_save)
     plot_total_carbon_emissions_timeseries(fileName, Data, dpi_save)
     plot_total_flow_carbon_emissions_timeseries(fileName, Data, dpi_save)
-    #plot_chi(fileName, Data, dpi_save)
-    #plot_omega(fileName, Data, dpi_save)
-    #plot_consum_ratio(fileName, Data, dpi_save)
-    #plot_L(fileName, Data, dpi_save)
-    #plot_H(fileName, Data, dpi_save)
-    #plot_Z_timeseries(fileName, Data, dpi_save)
-
-    """
-    if Data.network_type =="SBM":
-        plot_SBM_low_carbon_preferences_timeseries(fileName, Data, dpi_save)
-        plot_SBM_network_start_preferences(fileName, Data,cmap, dpi_save, node_sizes,norm_zero_one,block_markers_list,legend_loc,lines_alpha)
-        plot_SBM_network_end_preferences(fileName, Data,cmap, dpi_save, node_sizes,norm_zero_one,block_markers_list,legend_loc,lines_alpha)
-    else:
-        plot_low_carbon_preferences_timeseries(fileName, Data, dpi_save)
-        plot_network_start_preferences(fileName, Data,cmap, dpi_save, node_sizes,norm_zero_one)
-        plot_network_end_preferences(fileName, Data,cmap, dpi_save, node_sizes,norm_zero_one)
-    """
-    #threshold_list = [0.0001,0.0002,0.0005,0.001,0.002,0.003,0.004]
-    #emissions_threshold_range = np.arange(0,0.005,0.000001)
-    #plot_low_carbon_adoption_timeseries(fileName, Data,threshold_list, dpi_save)
-    #plot_low_carbon_adoption_transition_timeseries(fileName, Data,emissions_threshold_range, dpi_save)
-    
-    #anim_1 = variable_animation_distribution(fileName, Data, "history_identity","Identity", dpi_save,anim_save_bool)
-    #anim_2 = varaible_animation_distribution(fileName, Data, "history_flow_carbon_emissions","Individual emissions" , dpi_save,anim_save_bool)
-
-    #anim_3 = fixed_animation_distribution(fileName, Data, "history_identity","Identity","y", dpi_save,anim_save_bool)
-    #anim_4 = fixed_animation_distribution(fileName, Data, "history_flow_carbon_emissions","Individual emissions","y",dpi_save,anim_save_bool)
-    #anim_5 = fixed_animation_distribution(fileName, Data, "history_utility","Utility","y",dpi_save,anim_save_bool)
-    
-    #VECTORS
-    #anim_4 = multi_col_fixed_animation_distribution(fileName, Data, "history_low_carbon_preferences","Low carbon Preferences","y", dpi_save,anim_save_bool)
-
 
     plt.show()
 
