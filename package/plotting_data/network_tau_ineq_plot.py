cc9ad273ab12751f55f1302ad21342ddebfd2c19
Daniel Torren Peraire <1621511@uab.cat>
2025-06-18 00:01:13 +0200

    single seed

--- package/plotting_data/network_tau_ineq_plot.py
+++ package/plotting_data/network_tau_ineq_plot.py
@@ -129,5 +129,5 @@ def main(
 
 if __name__ == '__main__':
     plots = main(
-        fileName= "results/network_ineq_tau_23_46_00__17_06_2025"#network_ineq_tau_17_24_33__17_06_2025"#network_ineq_tau_11_50_31__17_06_2025"#network_ineq_tau_10_27_38__17_06_2025"#network_ineq_tau_11_59_40__11_06_2025"
+        fileName= "results/network_ineq_tau_23_50_10__17_06_2025"#network_ineq_tau_17_24_33__17_06_2025"#network_ineq_tau_11_50_31__17_06_2025"#network_ineq_tau_10_27_38__17_06_2025"#network_ineq_tau_11_59_40__11_06_2025"
     )
