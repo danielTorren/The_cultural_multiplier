f496a72133957c18309d3b8930287c9ef07a5b04
Daniel Torren Peraire <1621511@uab.cat>
2025-07-10 12:27:47 +0200

    simple h_min

--- package/plotting_data/simple_min_expen_plot.py
+++ package/plotting_data/simple_min_expen_plot.py
@@ -17,7 +17,7 @@ def plot_emissions_vs_min_expenditure(fileName):
     # Load data
     emissions_data = load_object(fileName + "/Data", "emissions_data_min_expenditure")
     variable_parameters_dict = load_object(fileName + "/Data", "variable_parameters_dict")
-    min_expenditure_shares = variable_parameters_dict["col"]["property_vals"]
+    min_expenditure_shares = variable_parameters_dict["property_vals"]
     network_labels = ["Small-World", "SBM", "Scale-Free"]
 
     num_networks = len(emissions_data)
@@ -52,4 +52,4 @@ def plot_emissions_vs_min_expenditure(fileName):
 
 # Example usage
 if __name__ == "__main__":
-    plot_emissions_vs_min_expenditure("results/simple_h_min_expenditure_10_07_2025")  # adjust path as needed
+    plot_emissions_vs_min_expenditure("results/simple_h_min_expenditure_12_21_57__10_07_2025")  # adjust path as needed
