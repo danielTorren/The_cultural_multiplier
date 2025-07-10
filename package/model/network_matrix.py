91b3663c6cd3fa280ca7a95ed8aea53e96bfdb05
Daniel Torren Peraire <1621511@uab.cat>
2025-07-10 17:05:38 +0200

    run big single

--- package/model/network_matrix.py
+++ package/model/network_matrix.py
@@ -446,7 +446,6 @@ class Network_Matrix:
                 self.carbon_price_m = self.carbon_price_increased_m
 
         self.prices_high_carbon_instant = self.prices_high_carbon_m + self.carbon_price_m
-        print(self.t, ": ", self.prices_high_carbon_instant)
 
     def _initialize_substitutabilities(self):
         """
